139a3bfa06ce061eb20a741c8f56f11ea50d9257
Joe Busillo <jbusillo@yahoo.com>
2020-04-04 22:40:46 -0500

    before adding Router

--- src/deck.js
+++ src/deck.js
@@ -1,4 +1,4 @@
-//import { app, io } from './server';
+import winston from 'winston';
 import { PokerDeck } from './pokerdeck';
 import { players } from './players';
 
@@ -6,20 +6,13 @@ let deck = [];
 let deckPointer = 0;
 
 export function init() {
-	// io.on('connection', function(socket) {
-	// 	console.log('io.on.connection');
-	// 	socket.emit('players', players);
-	// 	// socket.on('my other event', function(data) {
-	// 	// 	console.log(data);
-	// });
-
 	deck = shuffle();
 
 	let disp = '';
 	deck.forEach(cd => {
 		disp = disp + cd + ':';
 	});
-	console.log(disp);
+	winston.info('Shuffled Deck: %s', disp);
 }
 
 export function shuffle() {
@@ -54,5 +47,74 @@ function burn() {
 }
 
 export function draw() {
-	return deck[deckPointer++];
+	let card = {
+		short: deck[deckPointer],
+		long: getCardLongDescription(deck[deckPointer]),
+	};
+	deckPointer++;
+	return card;
+}
+
+function getCardLongDescription(short) {
+	let desc = '';
+	switch (short.substr(1)) {
+		case '02':
+			desc = 'Two';
+			break;
+		case '03':
+			desc = 'Three';
+			break;
+		case '04':
+			desc = 'Four';
+			break;
+		case '05':
+			desc = 'Five';
+			break;
+		case '06':
+			desc = 'Six';
+			break;
+		case '07':
+			desc = 'Seven';
+			break;
+		case '08':
+			desc = 'Eight';
+			break;
+		case '09':
+			desc = 'Nine';
+			break;
+		case '10':
+			desc = 'Ten';
+			break;
+		case '11':
+			desc = 'Jack';
+			break;
+		case '12':
+			desc = 'Queen';
+			break;
+		case '13':
+			desc = 'King';
+			break;
+		case '14':
+			desc = 'Ace';
+			break;
+	}
+
+	desc += ' of ';
+
+	switch (short.substr(0, 1)) {
+		case 'C':
+			desc += 'Clubs';
+			break;
+		case 'D':
+			desc += 'Diamonds';
+			break;
+		case 'H':
+			desc += 'Hearts';
+			break;
+		case 'S':
+			desc += 'Spades';
+			break;
+	}
+
+	return desc;
 }
--- src/players.js
+++ src/players.js
@@ -1,8 +1,9 @@
 import { app, io } from './server';
 import { v4 as uuidv4 } from 'uuid';
 import * as Deck from './deck';
+import winston from 'winston';
 
-let playerData = { players: [], dealer: null, seq: [] };
+let Players = { players: [], dealer: null, seq: [] };
 
 function init() {
 	// add the route
@@ -10,34 +11,68 @@ function init() {
 		addPlayer(req, res);
 	});
 
+	app.get('/startTable', (req, res) => {
+		startTable(req, res);
+	});
+
+	app.get('/doButton', (req, res) => {
+		doButton(req, res);
+	});
+
+	app.get('/playerReady', (req, res) => {
+		playerReady(req, res);
+	});
+
 	// add the channel (notify when 'players' has changed
-	io.on('connection', socket => {
-		socket.emit('playerData', playerData);
+	io.on('connection', (socket) => {
+		socket.emit('playerData', publicPlayerData());
 	});
 }
 
-function determineDealer() {
-	io.emit('myActionsData', 'freeze');
-	let spot = 1;
-	let card = 'XXxX';
-	Deck.shuffle();
-	while (card.substr(1, 2) !== '14') {
-		let card = Deck.draw();
+function doButton(req, res) {
+	try {
+		winston.info(`in doButton`);
+		startTable(req, res);
+		res.send(JSON.stringify({ status: 'OK' }));
+	} catch (e) {
+		winston.error('in doButton %s', e);
+		res.send(JSON.stringify({ status: 'Error', message: e }));
+	}
+}
+
+function playerReady(req, res) {
+	try {
+		winston.info(`playerReady, uuid = ${req.query.uuid}`);
+		let uuid = req.query.uuid;
+
+		let player = Players.players.find((e) => e.uuid === uuid);
+
+		io.emit('PokerMessage', 'GameStatus', {
+			message: `${player.name} joined the game!!!`,
+		});
+
+		io.emit('PokerMessage', 'PlayerStatus', publicPlayerData());
+
+		io.emit(uuid, 'MyActions', { buttons: [{ id: 'myStart', text: 'StartGame', bguid: 'testing' }] });
+
+		res.send(JSON.stringify({ status: 'OK' }));
+	} catch (e) {
+		winston.error('in playerReady %s', e);
+		res.send(JSON.stringify({ status: 'Error', message: e }));
 	}
 }
 
 function addPlayer(req, res) {
 	try {
-		console.log('addPlayer');
+		winston.info(`'addPlayer req query uuid = ${req.query.uuid}`);
 		let uuid = req.query.uuid;
 		if (!uuid) uuid = uuidv4();
 
-		const uuidNew = el => {
-			el.uuid !== uuid;
-		};
+		let player = Players.players.find((e) => e.uuid === uuid);
 
-		if (playerData.players.every(uuidNew)) {
-			playerData.players.push({
+		if (!player) {
+			winston.info(`'adding player to table, uuid = ${uuid}`);
+			Players.players.push({
 				uuid,
 				name: req.query.player,
 				money: 0,
@@ -46,21 +81,77 @@ function addPlayer(req, res) {
 				dealer: false,
 				thisHand: 'out', //out, in, ante-up, fold, call, raise $x.xx
 				nextHand: 'out',
+				cards: [],
+				actions: [],
 			});
 
-			playerData.seq.push(uuid);
+			Players.seq.push(uuid);
 		}
 
-		io.emit('PokerMessage', 'GameStatus', {
-			message: `${req.query.player} joined the game!!!`,
-		});
-
-		io.emit('PokerMessage', 'PlayerStatus', playerData);
 		res.send(JSON.stringify({ status: 'OK', uuid }));
 	} catch (e) {
-		console.log(e);
+		winston.error('in addPlayer %s', e);
 		res.send(JSON.stringify({ status: 'Error', message: e }));
 	}
 }
 
-module.exports = { init, addPlayer, playerData };
+function publicPlayerData() {
+	let publicData = [];
+	Players.players.forEach((el) => {
+		publicData.push({
+			name: el.name,
+			money: el.money,
+			winnings: el.winnings,
+			buyin: el.buyin,
+			dealer: el.dealer,
+			thisHand: el.thisHand, //out, in, ante-up, fold, call, raise $x.xx
+			nextHand: el.nextHand,
+		});
+	});
+	return publicData;
+}
+
+async function startTable() {
+	try {
+		winston.info(`in startTable`);
+
+		let card;
+
+		io.emit('PokerMessage', 'GameStatus', {
+			message: `Shuffling the deck...`,
+		});
+
+		Deck.shuffle();
+		let aceFound = false;
+
+		//Round robin until Ace is found
+		while (!aceFound) {
+			for (let i = 0; i < Players.players.length; i++) {
+				let player = Players.players[i];
+				await sleep(500);
+				card = Deck.draw();
+				player.cards.push(card.short);
+				io.emit(player.uuid, 'MyCards', { cards: player.cards });
+				io.emit('PokerMessage', 'GameStatus', {
+					message: `${player.name} drew the ${card.long}`,
+				});
+				if (card.short.substr(1) === '14') {
+					aceFound = true;
+					player.dealer = true;
+					io.emit('PokerMessage', 'GameStatus', {
+						message: `We have a dealer: ${player.name}`,
+					});
+					break;
+				}
+			}
+		}
+	} catch (e) {
+		winston.error('in startTable %s', e);
+	}
+}
+
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+module.exports = { init, addPlayer, Players };
